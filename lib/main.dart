6ce8ecec17269ed21a959f61d59850c5de7f388d
Ahmed Ewaida <ahmedewaida234@gmail.com>
2024-07-17 07:52:17 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+main(){
+
+}
+// class SplashBody extends StatefulWidget {
+//   const SplashBody({super.key});
+//
+//   @override
+//   State<SplashBody> createState() => _SplashBodyState();
+// }
+//
+// class _SplashBodyState extends State<SplashBody> with SingleTickerProviderStateMixin{
+//   late AnimatedContainer  animatedContainer;
+//   late Animation<Offset> slidingAnimation;
+//   @override
+//   void initState() {
+//     initSlidingAnimation();
+//
+//     super.initState();
+//   }
+//   @override
+//
+//   void initSlidingAnimation(){
+//     animatedContainer =AnimationController(vsync: this,duration: const Duration(seconds: 4)) as AnimatedContainer;
+//     // animatedContainer.form
+//   }
+//   slidingAnimation =Tween<Offset>(begin: Offset(5, 6),end: Offset.zero).animate(animatedContainer);
+//   Widget build(BuildContext context) {
+//     return Text("Ahmed Ewaida");
+//   }
+// }
